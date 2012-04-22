1292ff440beaa62d2b9f6ef848613499d40b7ba0
Ralitsa Ivanova <ralizabgbg@gmail.com>
2012-04-23 02:11:01 +0300

    QuoteHTTPService

---
+++ NorrisJokesApp/QuoteDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuoteDetailsViewController.h
+//  NorrisJokesApp
+//
+//  Created by Rali on 4/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuoteDetailsViewController : UIViewController
+
+@end
